b8162824aeaabcd7312ec440d1492439eae6e73a
programxiaobai <sbo8711@outlook.com>
2017-12-01 11:03:16 +0800

    NodeJs学习笔记

--- http/http.js
+++ http/http.js
@@ -1,4 +1,4 @@
-var http = require('http');
+var http = require('http/http');
 
 
 http.createServer(function (req, res) {
@@ -10,7 +10,7 @@ http.createServer(function (req, res) {
     res.statusCode = 404;
     res.setHeader('Content-type', 'text/html;charset=utf-8');
     res.setHeader('name', 'a');
-    res.write(new Date().toString());
+    //res.write(new Date().toString());
     res.end();
 }).listen(8080, "localhost");
 

deb2efac1035bf40448ac10bd4e70c607db827e2
programxiaobai <sbo8711@outlook.com>
2017-12-01 10:23:38 +0800

    NodeJs学习笔记

--- http.js
+++ http.js
@@ -2,9 +2,11 @@ var http = require('http');
 
 
 http.createServer(function (req, res) {
+    //设置请求属性
     console.log(req.method);
     console.log(req.url);
     console.log(req.headers);
+    //设置响应属性
     res.statusCode = 404;
     res.setHeader('Content-type', 'text/html;charset=utf-8');
     res.setHeader('name', 'a');

28408a8050f10b800c51d3136ad9a4b9e8269e25
programxiaobai <sbo8711@outlook.com>
2017-12-02 18:46:47 +0800

    NodeJs学习笔记

---
+++ http/1.js
@@ -0,0 +1,6 @@
+﻿~function () {
+    document.querySelector('#a').addEventListener("click", function () {
+        //有乱码时用记事本更改一下编码
+        alert("别点我了");
+    });
+}();
--- http/http.js
+++ http/http.js
@@ -1,16 +1,52 @@
-var http = require('http/http');
+var http = require('http');
+var fs = require('fs');
+var mime = require('mime');
 
 
 http.createServer(function (req, res) {
     //设置请求属性
     console.log(req.method);
-    console.log(req.url);
-    console.log(req.headers);
-    //设置响应属性
-    res.statusCode = 404;
-    res.setHeader('Content-type', 'text/html;charset=utf-8');
-    res.setHeader('name', 'a');
-    //res.write(new Date().toString());
-    res.end();
+    var url = req.url;
+    if (url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/html;charset=utf-8');
+        res.setHeader('name', 'a');
+        console.log(req.headers);
+        fs.readFile('http/1.html', function (err, data) {
+            res.write(data);
+            res.end();
+        });
+    } else if (url === '/1.css') {
+        res.setHeader('Content-type', 'text/css;charset=utf-8');
+        res.setHeader('name', 'a');
+        console.log(req.headers);
+        fs.readFile('http/1.css', function (err, data) {
+            res.write(data);
+            res.end();
+        });
+    } else if (url === '/1.js') {
+        // res.setHeader('Content-type', 'application/x-javascript;charset=utf-8');
+        // res.setHeader('name', 'a');
+        // console.log(req.headers);
+        // fs.readFile('http/1.js', function (err, data) {
+        //     res.write(data);
+        //     res.end();
+        // });
+        static(url, res);
+    }
+
+    function static(url, res) {
+        res.statusCode = 200;
+        res.setHeader('Content-type', mime.getType(url) + ';charset=utf-8');
+        res.setHeader('name', 'a');
+        console.log(req.headers);
+        fs.readFile('http/' + url.slice(1), function (err, data) {
+            res.write(data);
+            res.end();
+        });
+    }
+
+    // res.write(new Date().toString());
+    // res.end();
 }).listen(8080, "localhost");
 

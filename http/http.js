76eb38f1a15c77062aba338efa87b59d8951a5ba
programxiaobai <sbo8711@outlook.com>
2017-12-05 14:48:56 +0800

    NodeJs学习笔记

--- http/http.js
+++ http/http.js
@@ -1,46 +1,41 @@
+//HTTP请求 B/S交互过程
+
 var http = require('http');
 var fs = require('fs');
 var mime = require('mime');
+var url = require('url');//对url进行处理，把字符串转换成对象/?name=aa&age=6
 
 
 http.createServer(function (req, res) {
     //设置请求属性
-    console.log(req.method);
-    var url = req.url;
-    if (url === '/') {
-        res.statusCode = 200;
+    //console.log(req.method);
+
+    //true表示query转换成对象
+    var urlObj = url.parse(req.url, true);
+
+    if (urlObj.pathname === '/') {
+        // res.statusCode = 200;
+        //Content-type指定响应内容的编码和类型
         res.setHeader('Content-type', 'text/html;charset=utf-8');
         res.setHeader('name', 'a');
         console.log(req.headers);
-        fs.readFile('http/1.html', function (err, data) {
+        fs.readFile('1.html', function (err, data) {
             res.write(data);
             res.end();
         });
-    } else if (url === '/1.css') {
-        res.setHeader('Content-type', 'text/css;charset=utf-8');
-        res.setHeader('name', 'a');
-        console.log(req.headers);
-        fs.readFile('http/1.css', function (err, data) {
-            res.write(data);
-            res.end();
-        });
-    } else if (url === '/1.js') {
-        // res.setHeader('Content-type', 'application/x-javascript;charset=utf-8');
-        // res.setHeader('name', 'a');
-        // console.log(req.headers);
-        // fs.readFile('http/1.js', function (err, data) {
-        //     res.write(data);
-        //     res.end();
-        // });
-        static(url, res);
+        // static(url, res);
+    } else if (urlObj.pathname === '/1.css') {
+        static(urlObj.pathname, res);
+    } else if (urlObj.pathname === '/1.js') {
+        static(urlObj.pathname, res);
     }
 
     function static(url, res) {
-        res.statusCode = 200;
+        // res.statusCode = 200;
         res.setHeader('Content-type', mime.getType(url) + ';charset=utf-8');
         res.setHeader('name', 'a');
-        console.log(req.headers);
-        fs.readFile('http/' + url.slice(1), function (err, data) {
+        // console.log(req.headers);
+        fs.readFile(url.slice(1), function (err, data) {
             res.write(data);
             res.end();
         });

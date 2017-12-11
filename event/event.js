6b1bc680d008381fc5fe33bf44cadea522cbfaf9
programxiaobai <sbo8711@outlook.com>
2017-12-11 16:51:27 +0800

    event

--- event/event.js
+++ event/event.js
@@ -36,7 +36,14 @@ function Person(name) {
 //     console.log('b');
 // });
 // girl.emit('a');
+Person.prototype.once = function (eventName, callback) {
+    function onceCallBack() {
+        callback.apply(this, arguments);
+        this.removeListener(eventName, onceCallBack)
+    }
 
+    this.on(eventName, onceCallBack);
+};
 
 var EventEmitter = require('events');
 var util = require('util');
@@ -49,14 +56,63 @@ boy.on('boy', function () {
 boy.addListener('boy', function () {
     console.log('boy2');
 });
-var b = function () {
-    console.log('boy3');
+var b = function (style, s) {
+    console.log(style, s);
 };
 boy.once('b', b);
 boy.emit('boy');
-boy.removeListener('b', b);
-boy.emit('b');
-boy.emit('b');
+//boy.removeListener('b', b);
+boy.emit('b', '1', '2');
+// boy.emit('b');
+
+
+//¿ÉÇ¶Ì×£¬µ«Âé·³
+var fs = require('fs');
+var person = {};
+var eve = new EventEmitter();
+
+eve.on('data', out);
+
+fs.readFile('name.txt', 'utf8', function (err, data) {
+    person.name = data;
+    eve.emit('data');
+
+});
+fs.readFile('age.txt', 'utf8', function (err, data) {
+    person.age = data;
+    eve.emit('data');
+});
+
+function out() {
+    if (person.name && person.age) {
+        console.log(person.name, person.age);
+    }
+}
 
 
 
+function say(name,word) {
+    console.log(name,':',word);
+}
+// var newSay = say.bind(this,'a');
+function newSay() {
+    say.apply(null, ['a'].concat(Array.prototype.slice.call(arguments)));
+}
+newSay('11');
+
+function eat(times,callback) {
+    return function () {
+        if(times === 1){
+            callback();
+        }
+        times--;
+    };
+}
+var newEat = eat(5, function () {
+   console.log('over');
+});
+newEat();
+newEat();
+newEat();
+newEat();
+newEat();
